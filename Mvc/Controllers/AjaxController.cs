ddf7a9a50b8d7fe6818b2cba1bbfb77ad594a6c9
Richard Wilde <richard@wildesoft.net>
2015-03-26 14:43:40 +0000

    Cascading dropdown

--- Mvc/Controllers/AjaxController.cs
+++ Mvc/Controllers/AjaxController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace React.Test.Controllers
@@ -11,12 +12,32 @@ namespace React.Test.Controllers
             {
                 new {id = 0, value = "select"},
                 new {id = 1, value = "one"},
-                new { id = 2, value = "two" },
-                new { id = 3, value = "three" },
-                new { id = 4, value = "four" }
+                new {id = 2, value = "two" },
+                new {id = 3, value = "three" },
+                new {id = 4, value = "four" }
             };
 
             return Json(model, JsonRequestBehavior.AllowGet);
         }
+
+        public JsonResult Lookup2(int id)
+        {
+            var model = new List<dynamic>
+            {
+                new {id = 20, lookupid = 0, value = "select"},
+                new {id = 21, lookupid = 1, value = "one.1"},
+                new {id = 22, lookupid = 1, value = "one.2" },
+                new {id = 23, lookupid = 1, value = "one.3" },
+                new {id = 24, lookupid = 2, value = "two.1" },
+                new {id = 25, lookupid = 2, value = "two.2" },
+                new {id = 26, lookupid = 3, value = "three.1" },
+                new {id = 27, lookupid = 4, value = "four.1" },
+                new {id = 28, lookupid = 4, value = "four.2" },
+                new {id = 29, lookupid = 4, value = "four.3" }
+            }.Where(w=>w.lookupid == id);
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
---
+++ Mvc/Scripts/Pages/Dropdown.Ajax.Cascade.js
@@ -0,0 +1,114 @@
+﻿var MyParentCascade = React.createClass({
+
+    getInitialState: function() {
+        return {
+            data1: [], data2: [], 
+            value1: {}, value2: {}, 
+            showOutput: false
+        }
+    },
+
+    componentDidMount: function() {
+        $.get(this.props.source, function(result) {
+            this.setState({
+                data1: result
+            });
+        }.bind(this));
+    },
+
+    render: function() {
+        return (
+            <div onChange={this.changeHandler}>
+                <MySelectCascade 
+                    data={this.state.data1} 
+                    name="input1" />
+                <MySelectCascade 
+                    data={this.state.data2} 
+                    name="input2" />
+                { this.state.showOutput ? <MyOutputCascade item1={this.state.value1} item2={this.state.value2}/> : null }
+            </div>
+        )
+    },
+
+    changeHandler: function(e) {
+
+        if (e.target.name === "input1") {
+            console.log("parent changed");
+
+            this.state.data1.forEach(function(item) {
+                if (parseInt(item.id) === parseInt(e.target.value)) {
+                    this.setState({ showOutput: item.id > 0 });
+                    this.setState({ value1: item });
+
+
+                    $.get("/ajax/lookup2/" + item.id, function(result) {
+                        this.setState({
+                            data2: result
+                        });
+                        this.setState({ value2: result[0] });
+                    }.bind(this));
+
+                }
+            }.bind(this));
+        }
+
+        if (e.target.name === "input2") {
+            console.log("child changed");
+
+            this.state.data2.forEach(function(item) {
+                if (parseInt(item.id) === parseInt(e.target.value)) {
+                    this.setState({ showOutput: item.id > 0 });
+                    this.setState({ value2: item });
+                }
+            }.bind(this));
+        }
+
+    }
+});
+
+
+var MyOutputCascade = React.createClass({
+    
+    render: function() {
+        return (
+            <div>
+                <h3>Output</h3>
+                <p>Id: <b>{this.props.item1.id}</b> Value: <b>{this.props.item1.value}</b></p>
+                <p>Id: <b>{this.props.item2.id}</b> Value: <b>{this.props.item2.value}</b></p>
+            </div>
+        )
+    }
+
+});
+
+
+var MySelectCascade = React.createClass({
+
+    render: function() {
+        var mySelectOptions = function(result) {
+     
+            return <MySelectOptionsCascade
+                        key={result.id} 
+                        data={result} />
+            };
+            return (
+                <select
+                    name={this.props.name}
+                    className="form-control">
+                        {this.props.data.map(mySelectOptions)}
+                </select>
+            )
+    }
+});
+
+var MySelectOptionsCascade = React.createClass({
+    render: function() {
+        return <option value={this.props.data.id}>{this.props.data.value}</option>
+    }
+});
+
+
+React.render(
+    <MyParentCascade source="/ajax/lookup" />, 
+    document.getElementById('dropdowncascade')
+);
