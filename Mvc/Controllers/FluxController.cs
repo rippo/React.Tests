1610a9a63184493087f968d4e591e3c8e821602e
Richard Wilde <richard@wildesoft.net>
2015-03-28 08:51:17 +0000

    Added mcFly test (Flux implmentation)

---
+++ Mvc/Controllers/FluxController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace React.Test.Controllers
+{
+    public class FluxController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
